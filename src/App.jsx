b88f2a44d5748a03ae33e7e3acb7c6e71f82910f
Jere Lantz <lantz.jere@gmail.com>
2025-03-11 19:40:21 +0200

    made the game board logic better

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import {useState} from "react"
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
+import Log from "./components/Log"
 
 function App() {
+    const [gameTurns, setGameTurns] = useState([])
     const [activePlayer, setActivePlayer] = useState("X")
 
-    function handleActivePlayer(){
+    function handleActivePlayer(rowIndex, colIndex){
         setActivePlayer((curActivePlayer)=> curActivePlayer == "X" ? "O" : "X")
+        setGameTurns(prevTurns => {
+            let currentPlayer = "X"
+
+            if(prevTurns.length > 0 && prevTurns[0].player == "X"){
+                currentPlayer = "O"
+            }
+            const updatedTurns = [{square:{row:rowIndex, col:colIndex},
+                player:currentPlayer}, ...prevTurns]
+
+            return updatedTurns
+        })
     }
 
   return (
@@ -16,9 +29,9 @@ function App() {
       <Player initialName="Player 1" symbol="X" isActive={activePlayer == "X"}/>
       <Player initialName="Player 2" symbol="O" isActive={activePlayer == "O"}/>
       </ol>
-      <GameBoard onSelectSquare={handleActivePlayer} activePlayerSymbol={activePlayer}/>
+      <GameBoard onSelectSquare={handleActivePlayer} turns={gameTurns}/>
       </div>
-      LOG
+      <Log />
       </main>
   )
 }

1ec955599c31c6cef4a4216bc1ac68e4324219f7
Jere Lantz <lantz.jere@gmail.com>
2025-03-11 09:17:13 +0200

    The player turn tracking works. Active player is highlighted and it draws the correct symbol to the board

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react"
+
 const initialGameBoard = [
     [null,null,null],
     [null,null,null],
     [null,null,null]
 ]
-export default function GameBoard(){
+export default function GameBoard({onSelectSquare, activePlayerSymbol}){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+    function handleSelection(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(inArr => [...inArr])]
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol
+            return updatedBoard
+        })
+
+        onSelectSquare()
+    }
+
     return(
         <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+        {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
             <ol>
             {row.map((col,colIndex) => (<li key={colIndex}>
-                <button>{col}</button>
+                <button onClick={()=>handleSelection(rowIndex,colIndex)}>{col}</button>
                 </li>))}
             </ol>
             </li>)}

55048a32af7d9f53e5dbb4c1c9fb5e03ddeb43ba
Jere Lantz <lantz.jere@gmail.com>
2025-03-12 15:26:30 +0200

    implemented the logic for checking for a winner

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
 import Log from "./components/Log"
 
+import { WINNING_COMBINATIONS } from "./winning-combinations"
+
+const initialGameBoard = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+]
+
 function deriveActivePlayer(gameTurns){
     let currentPlayer = "X"
     if(gameTurns.length > 0 && gameTurns[0].player == "X"){
@@ -16,6 +24,26 @@ function App() {
     //const [activePlayer, setActivePlayer] = useState("X")
 
     const activePlayer = deriveActivePlayer(gameTurns)
+
+    let gameBoard = initialGameBoard
+
+    for(const turn of gameTurns){
+        const {square, player} = turn
+        const {row,col} = square
+        gameBoard[row][col] = player
+    }
+
+    let winner
+
+    for(const combination of WINNING_COMBINATIONS){
+        const firstSqSymbol = gameBoard[combination[0].row][combination[0].column]
+        const secondSqSymbol = gameBoard[combination[1].row][combination[1].column]
+        const thirdSqSymbol = gameBoard[combination[2].row][combination[2].column]
+
+        if(firstSqSymbol && firstSqSymbol == secondSqSymbol && firstSqSymbol == thirdSqSymbol){
+            winner = firstSqSymbol
+        }
+    }
 
     function handleActivePlayer(rowIndex, colIndex){
         //setActivePlayer((curActivePlayer)=> curActivePlayer == "X" ? "O" : "X")
@@ -36,7 +64,8 @@ function App() {
       <Player initialName="Player 1" symbol="X" isActive={activePlayer == "X"}/>
       <Player initialName="Player 2" symbol="O" isActive={activePlayer == "O"}/>
       </ol>
-      <GameBoard onSelectSquare={handleActivePlayer} turns={gameTurns}/>
+      {winner && <p>You won, {winner}!</p>}
+      <GameBoard onSelectSquare={handleActivePlayer} board={gameBoard}/>
       </div>
       <Log turns={gameTurns}/>
       </main>
